9ef174f68bee845cf00cfe18e49e6acc0d28c16e
felipezbozne <zbozne11@gmail.com>
2026-09-01 07:13:44 -0300

    boolean

---
+++ 6-strings copy/js/scripts.js
@@ -0,0 +1,19 @@
+var verdadeiro = true;
+
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+
+console.log(typeof true);
+console.log(typeof false);
+
+var falso = false;
+
+console.log(falso);
+console.log(typeof falso);
+
+let maiorDeIdade = true;
+if (maiorDeIdade) {
+console.log("Você pode tirar carteira de motorista.");
+} else {
+console.log("Você ainda não pode tirar carteira de motorista.");
+}
