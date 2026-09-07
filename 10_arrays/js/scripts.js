091bd0bee5d7957fec9d6945daf95d84a7ff0cc3
felipezbozne <zbozne11@gmail.com>
2026-09-07 00:11:52 -0300

    array e obj

---
+++ 10_arrays/js/scripts.js
@@ -0,0 +1,16 @@
+var arr =[5, "luis",  true, {teste: 1}]; // obj=> {}
+
+console.log(arr);
+
+    
+var arr2 = [1,2,3,4];
+
+console.log(arr2);
+
+arr2[4] = 5;
+
+arr2[0] = "teste";
+
+console.log(arr2);
+
+console.log(typeof arr)
---
+++ 9_objetos/js/scripts.js
@@ -0,0 +1,13 @@
+var obj = {
+    nome: "Luis",
+    idade: 18,
+    profissao: "almoxarife"
+};
+
+console.log(obj);
+
+console.log(typeof obj);
+
+console.log(obj.nome);
+
+console.log("Meu nome é " + obj.nome);
