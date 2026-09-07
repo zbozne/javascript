91ca55397ff8e1ea30ac2c18e34139f1ae68c3f4
felipezbozne <zbozne11@gmail.com>
2026-09-07 01:58:43 -0300

    else e elseif

--- 13_else_if_e_else/js/scripts.js
+++ 13_else_if_e_else/js/scripts.js
@@ -1,25 +1,19 @@
-var idade = 18;
-var possuiCarro = 1
-
-if(idade => 18) {
-    console.log("O usuario pode fazer a carteira");
-}
-
-if(idade <= 17) {
-    console.log("O usuario nao pode fazer a carteira");
-}
-
-if(possuiCarro) {
-    console.log("O usuario ja pode andar de carro");
-}
-
-var nome = "Luis"
+var nome = "Vitor";
 
 if(nome == "Luis") {
-    console.log("Seu nome é Luis");
+    console.log("O nome dele é Luis"); 
+ }  else if(nome == "Matheus") {
+        console.log("o nome é Matheus");   
+    }
+ else {
+    console.log("Ele possui outro nome");
 }
 
-if(nome != "Luis") {
-    console.log("Seu nome não é Luis")
-}
+var idade = 20;
 
+if(idade >= 20) {
+    console.log("Pode entrar na festa");
+} 
+else if(idade =>18) {
+    console.log("Ele pode entrar na festa com autorizacao");
+}
