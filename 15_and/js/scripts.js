68de30aa70ace0e46c44e7d60742fb90e897104b
felipezbozne <zbozne11@gmail.com>
2026-09-07 16:29:07 -0300

    or and e etc

---
+++ 15_and/js/scripts.js
@@ -0,0 +1,9 @@
+var idade = 16;
+var nome = "João";
+
+if(nome == "João" && idade == 15) {
+    console.log("O João pode entrar no curso");
+} else {
+    console.log("Este não é o João");
+}
+    
---
+++ 16_or/js/scripts.js
@@ -0,0 +1,10 @@
+var idade = 12
+var nome = "Pedro";
+
+if(nome == "João" || idade > 14) {
+    console.log("O João pode entrar no curso");
+} else {
+    console.log("Não pode entrar")
+}
+
+    
