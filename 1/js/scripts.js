7a1b3ab2a2db9b7f1650c9f8748cb81b36441beb
felipezbozne <zbozne11@gmail.com>
2026-09-07 01:11:17 -0300

    else e == etc

---
+++ 1/js/scripts.js
@@ -0,0 +1,25 @@
+var idade = 18;
+var possuiCarro = 1
+
+if(idade => 18) {
+    console.log("O usuario pode fazer a carteira");
+}
+
+if(idade <= 17) {
+    console.log("O usuario nao pode fazer a carteira");
+}
+
+if(possuiCarro) {
+    console.log("O usuario ja pode andar de carro");
+}
+
+var nome = "Luis"
+
+if(nome == "Luis") {
+    console.log("Seu nome é Luis");
+}
+
+if(nome != "Luis") {
+    console.log("Seu nome não é Luis")
+}
+
